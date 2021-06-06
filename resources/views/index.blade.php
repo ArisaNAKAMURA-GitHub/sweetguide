86f06b0bcd5cddb7b79e6c267def2936b0722239
Atsuko Inoue <sinba.805i@gmail.com>
2021-06-06 18:26:19 +0700

    Review Create Server側の表示機能をIndex .blade.phpに追加
    画像表示とレビュー星の平均は一旦保留でPRしています。

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,38 +22,38 @@
             </div>
         </div>
 
-        {{-- @foreach ($Stores as $detail) --}}
+        @foreach ($Stores as $detail)
         <div class="review">
             <div class="storephoto">
-                <a href=""><img src="" alt="店舗写真"></a>
+                <a href=""><img src="{{?????あとで書く}}" alt="店舗写真"></a>
             </div>
             <div class="add">
                 <div class="word font">
                     <p>お気に入りに追加</p>
                 </div>
                 <a href=""><img src="./css/rogo_okiniiri.jpg" alt="お気に入り星" class="rogo"></a>
-                <a href=""><img src="./css/rogo_addreview.jpg" alt="口コミ鉛筆" class="rogo"></a>
+                <a href="{{route('sweets.create')}}"><img src="./css/rogo_addreview.jpg" alt="口コミ鉛筆" class="rogo"></a>
                 <div class="word font">
                     <p>口コミを投稿する</p>
                 </div>
             </div>
             <div class="info font">
-                <p>（店舗名引っ張る）</p>
-                <p>（住所引っ張る）</p>
-                <p>（カテゴリ引っ張る）</p>
-                <p>（価格引っ張る）</p>
-                <p>（評価のポイント平均引っ張る）</p>
-                <button><a href="">詳細</a></button>
+                <p>{{$store->name}}（店舗名引っ張る）</p>
+                <p>{{$store->address}}（住所引っ張る）</p>
+                <p>{{$store->category_id}}（カテゴリ引っ張る）</p>
+                <p>{{$store->price}}（価格引っ張る）</p>
+                {{-- 一旦保留<p>（評価のポイント平均引っ張る）</p> --}}
+                <button><a href="{{route('sweets.show', $store->id)}}">詳細</a></button>
             </div>
             <div class="comment font">
-                <p>（ユーザーネーム引っ張る）さんの口コミ</p>
-                <p>投稿日時：（create時間引っ張る）</p>
-                <p>（評価引っ張る）</p>
-                <p>（コメント引っ張る）</p>
-                <button><a href="">続きを見る・・・</a></button>
+                <p>（{{$user->name}}ユーザーネーム引っ張る）さんの口コミ</p>
+                <p>投稿日時：{{$review->published_at}}（create時間引っ張る）</p>
+                <p>{{$review->review}}（評価引っ張る）</p>
+                <p>{{$review->comment}}（コメント引っ張る）</p>
+                <button><a href="{{route('sweets.show', $review->id)}}">続きを見る・・・</a></button>
             </div>
         </div>
-        {{-- @endforeach --}}
+        @endforeach
     </div>
 
 
